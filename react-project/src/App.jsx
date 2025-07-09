6e29a81688b91a91905d3f1f622a97d819716785
Your Name <jomerubaldo@gmail.com>
2025-07-10 02:36:57 +0800

    initial commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,19 +1,33 @@
 function App() {
-  const studentInfo = {
-    name: 'Jomer',
-    age: 21,
-    course: 'BSIT',
-  };
-
-  const myFormat = (studentInfo) => {
-    // need pala maglagay ng parameter from variable ng object
-    return studentInfo.name + ' ' + studentInfo.age + ' ' + studentInfo.course;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.elements.fname.value);
+    console.log(event.target.elements.lname.value);
   };
 
   return (
-    // copy paste lang yong variable name ng arrow function tapos yong parameter niya using curley braces
     <>
-      <h1>{myFormat(studentInfo)}</h1>
+      <form className="w-25 p-5" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="fname"
+            placeholder="enter firstname"
+            className=""
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="lname"
+            placeholder="enter lastname"
+            className=""
+          />
+        </div>
+        <button className="bg-indigo-500 px-10 py-2" type="submit">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
