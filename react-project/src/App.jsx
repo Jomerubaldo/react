446d81eea1e0a30822480d37d2aee76424834581
Your Name <jomerubaldo@gmail.com>
2025-07-09 16:19:43 +0800

    initial commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 function App() {
+  const studentInfo = {
+    name: 'Jomer',
+    age: 21,
+    course: 'BSIT',
+  };
+
+  const myFormat = (studentInfo) => {
+    // need pala maglagay ng parameter from variable ng object
+    return studentInfo.name + ' ' + studentInfo.age + ' ' + studentInfo.course;
+  };
+
   return (
-    <div className="flex justify-center items-center bg-black w-screen h-screen">
-      <div className="bg-white w-52 h-52 ">
-        <h1 className="text-center text-red-500">Kupal kaba boss</h1>
-      </div>
-    </div>
+    // copy paste lang yong variable name ng arrow function tapos yong parameter niya using curley braces
+    <>
+      <h1>{myFormat(studentInfo)}</h1>
+    </>
   );
 }
 
