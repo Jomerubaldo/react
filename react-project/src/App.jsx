0384ae8e7ba295324bbc5a361a59a267bf555cb6
Your Name <jomerubaldo@gmail.com>
2025-07-11 00:51:40 +0800

    initial commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,12 @@
-import Input from './components/Input';
-
 function App() {
+  const handleClick = (name) => {
+    return alert(`Hello Dear, ${name}`);
+  };
+
   return (
-    //Here is component u can paste this your component then edit it using props tobe dynamic//
     <>
-      <Input name="Button1" color="bg-green-500 px-10 py-2 text-red-500" />
-      <Input name="Button2" color="bg-red-500 px-10 py-2 text-blue-500" />
-      <Input name="Button2" color="bg-indigo-500 px-10 py-2 text-green-500" />
+      <button onClick={() => handleClick('Jomer')}>Click Me!</button>
     </>
   );
 }
-
 export default App;
